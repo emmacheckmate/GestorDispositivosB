c176f73a61871c652bd4b50455f482c211d8cc5b
Emmanuel Escobar <32559572+emmacheckmate@users.noreply.github.com>
2021-05-27 16:56:36 -0500

    metodo de impresion
    impresion actualizacion

--- GestorDeDispositvos/CrudEmp.cs
+++ GestorDeDispositvos/CrudEmp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,11 @@ namespace GestorDeDispositvos
             txtNumEmp.Text = "";
 
 
+            new Impresion();
+
         }
+
+
+
     }
 }
