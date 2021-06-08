6bd066a1e4b7c3d7e7dd5b2ccd7d41705b9c4b52
Emmanuel Escobar <32559572+emmacheckmate@users.noreply.github.com>
2021-06-08 09:42:07 -0500

    clase base de datos
    Se agrego la clase para la base de datos, para controlar y hacer mejor las  consultas

---
+++ GestorDeDispositvos/BDSQL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GestorDeDispositvos
+{
+    class BDSQL
+    {
+        public DataTable dt;
+
+        string cadenacnx = "Data Source=DESKTOP-S0SCMU4" + "\\" + "SQLEXPRESS;Initial Catalog=dbGestDisp;Integrated Security=True";
+        private string cadeConexion;
+        private string Query;
+        
+
+        public string QuerySG
+        {
+            get => Query;
+            set => Query = value;
+        }
+        public string cdncnxSG
+        {
+            get => cadeConexion;
+            set => cadeConexion = value;
+        }
+
+        
+        public void inicializaSQLAdpater(string qry)
+        {
+          SqlDataAdapter da = new SqlDataAdapter( qry ,  cadeConexion );
+         
+        }
+    }
+}
