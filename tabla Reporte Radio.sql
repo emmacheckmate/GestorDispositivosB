8ddcbc59c8339f125d67852117d1f1ca154e60ab
Emmanuel Escobar <32559572+emmacheckmate@users.noreply.github.com>
2021-07-02 00:12:34 -0500

    query

--- tabla Reporte Radio.sql
+++ tabla Reporte Radio.sql
@@ -4,14 +4,17 @@ CREATE TABLE reportesRadios
 	 fecha_asignacion date not null,
 	 observaciones varchar(100) not null, 
 	 numero_radio nchar(8) not null,
+	 
 	 resp char(10) not null ,
 	 sucursal nchar(10) not null,
-	 area int not null,
+	 area nchar(1) not null,
+	 estado nchar(1) not null,
 	 primary key (numero_reporte),
 	 foreign key ( numero_radio) references CatRadio(idRadio),
 	 foreign key ( resp ) references CatEmp( numEmp ),
 	 foreign key ( sucursal ) references catSucursal( idsuc ), 
-	 foreign key (area) references CatArea( clave_area)
+	 foreign key (area) references catArea( idArea ),
+	 foreign key ( estado) references catEdo( idEdo ),
 
       
 );
